85d47e59daa72ffdb95aebdee2d59f5279853552
Trent Patterson <jtrent238@outlook.com>
2018-02-06 09:51:19 -0500

    Create confighandler.lua

---
+++ MineTest/hammermod/confighandler.lua
@@ -0,0 +1,6 @@
+   -- configuration file for program `hammermod'
+    if toastercustombreaksound("ToasterCustomBreakSound") == "false" then
+      minetest.log([info,] "ToasterCustomBreakSound = false")
+    else
+      minetest.log([info,] "ToasterCustomBreakSound = true")
+    end
