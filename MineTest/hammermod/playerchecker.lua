8ae28c6bc2890d0487596e16e030f781c7ab2438
Trent Patterson <jtrent238@outlook.com>
2018-02-06 09:45:57 -0500

    Update playerchecker.lua

--- MineTest/hammermod/playerchecker.lua
+++ MineTest/hammermod/playerchecker.lua
@@ -2,4 +2,4 @@
   if minetest.get_player_name() == "jtrent238" {
       minetest.log([info,] "It seems you have a custom hammer in the mod, lets get it for you. (NOTE: This is a test))
       }
-    else minetest.log([info,] "You do not have a custom hammer in the mod, this is still work in progress.)
+    else minetest.log([info,] "You do not have a custom hammer in the mod, this is still work in progress.")
